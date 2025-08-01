3dffb68592dba325659aadecdcd397d6230273e5
goforindu <goforindu@gmail.com>
2025-08-01 11:06:14 +0530

    some changes

--- src/components/QuoteSkeleton.jsx
+++ src/components/QuoteSkeleton.jsx
@@ -1,9 +1,9 @@
 const QuoteSkeleton = () => {
   return (
-    <div className="p-6 w-full bg-gradient-to-br from-blue-100/80 via-indigo-100/80 to-purple-100/80 rounded-xl shadow-xl border border-indigo-200 backdrop-blur-md animate-pulse space-y-3">
+    <div className="p-6 w-[150%] bg-gradient-to-br from-blue-100/80 via-indigo-100/80 to-purple-100/80 rounded-xl shadow-xl border border-indigo-200 backdrop-blur-md animate-pulse space-y-3">
       {/* Simulate 2–3 lines of quote text */}
 
-      <div className="h-5 bg-gray-300/60 rounded w-[200%]"></div>
+      <div className="h-5 bg-gray-300/60 rounded w-full"></div>
       <div className="h-5 bg-gray-300/60 rounded w-11/12"></div>
       <div className="h-5 bg-gray-300/60 rounded w-9/12"></div>
 
